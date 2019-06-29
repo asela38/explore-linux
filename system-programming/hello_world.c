40f6a2ae951c5080c902738662ab28ee266374d5
asela <asela38@gmail.com>
2019-06-30 01:04:55 +0800

    add error handling on file access

--- system-programming/hello_world.c
+++ system-programming/hello_world.c
@@ -1,9 +1,16 @@
 #include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void main() {
 
 	int fd;
-	fd = open("foo", O_WRONLY | O_CREAT, 0644);
+	if((fd = open("foo", O_WRONLY | O_CREAT, 0644)) < 0) {
+		printf("Error Occured #: %d \n", errno);
+		perror("foo");
+		exit(1);
+	}
 	write(fd, "Hello, World!\n", 15);
 	close(fd);
 }
